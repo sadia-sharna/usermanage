5649efe9687851fea8762f8a51a2d6941137ee4f
SadiaGithub <s.sharna06@gmail.com>
2020-11-01 14:57:51 +0600

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import jQuery from 'jquery/dist/jquery.min.js';
+import Popper from '@popperjs/core/dist/umd/popper.min.js';
+import Bootstrap from 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.css';
+import vPagination from 'vue-plain-pagination'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
